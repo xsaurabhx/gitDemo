8d9d84166c490975523a0a946afec33b83d6fc5d
saurabh <dorendezvous@gmail.com>
2019-10-15 11:43:56 +0100

    this is commit from demo side

--- src/test/java/nayaproject/restapiTest.java
+++ src/test/java/nayaproject/restapiTest.java
@@ -8,6 +8,8 @@ public class restapiTest {
 	    public void postJira()
 	    {
 	    	System.out.println("postJira");
+	    	System.out.println("postJira2");
+	    	System.out.println("postJira3");
 	    }
 	    
 	    @Test
